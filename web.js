4f27da2da6caed65c4083a4320952cedcbee35bd
vlightyear <vchulu@gmail.com>
2013-07-05 11:04:17 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
+var buf =  fs.readFileSync('index.html');
+var msg = buf.toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(msg);
 });
 
 var port = process.env.PORT || 5000;
